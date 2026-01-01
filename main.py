a2b3bdfe7320e55dd211e255f6af3bbfec812aae
Kshu27 <webflask27@gmail.com>
2026-01-01 14:40:55 +0530

    day 2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from hmac import new
 import random
+from telnetlib import STATUS
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -12,6 +13,7 @@ class post(BaseModel):
     content:str
     published:bool=False
     rating:Optional[int]=None
+    #no need of geberating id here as it will be generated in the create post function
 #name your app
 
 app=FastAPI()
@@ -39,12 +41,39 @@ def create_post(new_post:dict=Body(...)):
     return{"data":f"name is {new_post['name']}"}
 '''
 
-@app.post("/post")
+@app.post("/create_post",status_code=status.HTTP_201_CREATED)
 def create_post(new_post:post):
     post_dict=new_post.dict()
     post_dict['id']=randrange(0,1000000)
     my_post.append(post_dict)
     return{"data":post_dict}# will get a value error if  atleast oen part ios missing
 
+#brutforce method for getting sp[ecific post
+
+def find_post(id):
+    for p in my_post:
+        if p["id"]==id:
+            return p
+        
 
 
+@app.get("/post/{id}")
+def get_post_specific(id:int,response:Response):#validation check for id to be an integer
+    print(id)
+    post = find_post(id)#alternate method=> in basemodel id type is int and here in request id type is str so we need to convert
+    print(post)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"invalid id  try again later")#it is of fastapi
+        '''
+        alternate method
+        response.status_code=status.HTTP_404_NOT_FOUND
+        return {"alert!":f"post with id {id} not found"}
+        '''
+    return {"post detail": post}
+
+
+#get latest post
+@app.get("/latest_post")
+def get_latest_post():
+    latest_post=my_post[len(my_post)-1]
+    return{"latest post":latest_post}
